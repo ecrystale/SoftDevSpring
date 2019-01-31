0211f7a6369e68b508273fde95f893a4e285091e
ecrystale <elee19@stuy,edu>
2019-01-30 22:39:54 -0500

    K 00

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,4 +1,35 @@
-var c=document.getElementByID("slate");
+var c=document.getElementById("slate");
 var ctx=c.getContext("2d");
 
-ctx.fillRect(50,50,100,200);
+var toggle=document.getElementById('toggle');
+var rect=true;
+
+toggle.addEventListener('click',function(e){
+    if(rect==true){
+	rect=false;
+    }
+    else{
+	rect=true;
+    };
+    console.log(e);
+});
+
+
+c.addEventListener('click',function(e){
+    if(rect==false){
+	console.log(e);
+	ctx.fillStyle = 'black';
+	ctx.beginPath();
+	ctx.ellipse(event.pageX-8,event.pageY-165.7812,2,2,0,0,360);
+	ctx.fill();
+	ctx.stroke()
+    }
+    if(rect==true){
+	    console.log(e);
+	    ctx.fillRect(event.pageX-8,event.pageY-165.78125,100,50);
+    };
+});
+
+document.getElementById('clear').addEventListener('click',function(e){
+    ctx.clearRect(0,0,c.width,c.height);
+});
