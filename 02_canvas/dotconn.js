572d72126933cbbec6c2aaadb3e01ed65ce8237d
ecrystale <elee19@stuy.edu>
2019-02-01 13:19:31 -0500

    K02

---
+++ 02_canvas/dotconn.js
@@ -0,0 +1,38 @@
+/*
+  Orange -- Ahnaf Kazi, Emily Lee
+  SoftDev2 pd7
+  K#01 -- ...and I want to Paint It Better
+  2019-01-31
+*/
+
+
+c=document.getElementById("playground");
+ctx=c.getContext("2d");
+clear=document.getElementById("clear");
+
+//clears canvas
+clear.addEventListener("click",
+		       function(e){
+			   ctx.clearRect(0, 0, c.width, c.height);
+		       }
+		      );
+
+//draws shape on click on canvas
+
+var origX=-1;
+var origY=-1;
+
+c.addEventListener("click",
+		   function(e){		       
+		       //ctx.beginPath() creates a new path and empties the list of sub-paths, which leads to seperating of shapes from being in the same group.
+		       ctx.beginPath();
+		       ctx.ellipse(e.offsetX, e.offsetY, 15, 15, 0, 0, 2*Math.PI);
+		       ctx.fill();
+		       if (origX>=0){
+			   ctx.beginPath();
+			   ctx.lineTo(origX,origY);
+		       }
+		       origX=e.offsetX;
+		       origY=e.offsetY;
+		   }
+		  );
