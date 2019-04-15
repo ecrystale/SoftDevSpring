ec6739e501bf6dec5f35a57aabdc3842150c15ff
ecrystale <elee19@stuy.edu>
2019-04-14 22:37:07 -0400

    done

--- 17_listcomp/work.py
+++ 17_listcomp/work.py
@@ -3,20 +3,20 @@ single=[]
 for i in range(9):
     if i%2==0:
         single.append(str(i)*2)
-print(single)
+#print(single)
 
 single2=[str(i)*2 for i in range(9) if i%2==0]
 
-print(single2)
+#print(single2)
 
 #2
 seven=[]
 for i in range(5):
     seven.append(i * 10 + 7)
-print(seven)
+#print(seven)
 
 seven2 = [i * 10 + 7 for i in range(5)]
-print(seven2)
+#print(seven2)
 
 #3
 triple=[]
@@ -24,10 +24,10 @@ for i in range(3):
     for x in range(3):
         triple.append(x*i)
 
-print(triple)
+#print(triple)
 
 triple2=[x*i for x in range(3) for i in range(3)]
-print(triple2)
+#print(triple2)
 
 #4
 composite = []
@@ -37,9 +37,9 @@ for i in range(1,101):
             composite.append(i)
             break
 
-print(composite)
+#print(composite)
 
-composite2=[i for i in range(1,101) for x in range(2,i - 1) if i % x==0 and i != x]
+composite2=list(set([i for i in range(1,101) for x in range(2,i - 1) if i % x==0 and i != x ]))
 #print(composite2)
 
 #5
@@ -50,7 +50,10 @@ for i in range(1,101):
             prime.append(i)
         if i % x == 0 and i != x:
             break
-print(prime)
+#print(prime)
+
+prime2=[i for i in range(2,101) if all(i % x!=0 for x in range(2,i - 1))]
+#print(prime2)
 
 #6
 def DivNum(num):
@@ -60,12 +63,12 @@ def DivNum(num):
             divisor.append(i)
     return divisor
 
-print(DivNum(6))
+#print(DivNum(6))
 
 def DivNum2(num):
     return [i for i in range(1,num) if num % i==0]
 
-print(DivNum2(6))
+#print(DivNum2(6))
 
 #7
 def transpose(m):
@@ -78,7 +81,11 @@ def transpose(m):
     return m2
 
 m=[[0,1,2],[3,4,5],[6,7,8]]
-print(m)
-print(transpose(m))
+#print(m)
+#print(transpose(m))
 
+def transpose2(m):
+    return [[m[x][i] for x in range(len(m))] for i in range(len(m))]
 
+#print(m)
+#print(transpose2(m))
