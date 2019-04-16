9cd3c5c6b7e19506bcd734d2a383defcfdd4a0a6
ecrystale <elee19@stuy.edu>
2019-04-15 21:36:13 -0400

    fix triple

--- 18_listcomp/app.py
+++ 18_listcomp/app.py
@@ -1,5 +1,4 @@
-import math
 def pythag(n):
-    return [[x,y,int((x**2+y**2)**0.5)] for x in range(1,n+1) for y in range(1,n+1) if ((x**2+y**2)**0.5).is_integer()]
+    return [[x,y,int((x**2+y**2)**0.5)] for x in range(1,n+1) for y in range(1,n+1) if ((x**2+y**2)**0.5).is_integer() and x<=y]
 
 print(pythag(19))
