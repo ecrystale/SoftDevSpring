539848de573d1ec79649bac05610b57202bc0e1f
ecrystale <elee19@stuy.edu>
2019-04-18 13:17:24 -0400

    work on

--- 20_anon-reduce/app.py
+++ 20_anon-reduce/app.py
@@ -1,3 +1,4 @@
+import datetime
 from functools import reduce
 
 #read Grimm's Fairy Tails
@@ -23,16 +24,32 @@ print( find_1( "to" ) )
 
 # Find the total frequency of a group of words
 def find_mult( li ):
-    count_mult = [ 1 for x in words if x in li ]
+    count_mult = [ 1 for x in range(len(words)-len(li)+1) \
+                       for i in range(len(li)) \
+                       if " ".join([ y for y in words[x:len(li)+x] ]) \
+                       == " ".join([ y for y in li])]
 
+    if len( count_mult ) == 0:
+        return 0
+    
     return reduce( (lambda x, y: x + y), count_mult )
 
-print( find_mult( ["fox", "to"] ) )
+print( "finding: fox to -- {}".format( find_mult( ["fox"] ) ) )
+print( find_mult( ["and", "the"] ) )
 
 # Find the most frequently occurring word
 def findmost():
+    print(datetime.datetime.now())
+    
     allw=[]
     [allw.append(x) for x in words if x not in allw]
+
+    print(datetime.datetime.now())
+
     counts = [ [ x, find_1(x) ] for x in allw ]
+
+    print(datetime.datetime.now())
+
     return reduce( (lambda x, y: x if x[1] >= y[1] else y), counts )
+
 print( findmost() )
